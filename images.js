2d61034d7811ba7133f492916c72a4f2c1364a2a
Amr Essam <amressamm@gmail.com>
2024-11-02 21:57:35 +0200

    init2

--- images.js
+++ images.js
@@ -5,5 +5,13 @@ export default [
   "making.png",
   "sitting.jpg",
   "looking.jpg",
-  "staring.jpg"
+  "staring.jpg",
+  "info pic 1.jpg",
+  "info pic 2.jpg",
+  "info pic 3.jpg",
+  "info pic 4.jpg",
+  "info pic 5.jpg", 
+  "info pic 7.jpg",
+  "info pic 8.jpg",
+  "info pic 9.jpg",
 ];
